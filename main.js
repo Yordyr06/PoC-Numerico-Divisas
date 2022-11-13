8086bb627822f6e4a929cc189e1a68a0cdf1d567
Yordy <yoalmonte.dev@outlook.com>
2022-11-12 21:34:38 -0400

    proyecto terminado

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  })
+const currencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
 
-  var value = 10000
-
-  console.log(formatter.format(value)) // "$10,000
+const showCurrency = (value) => {
+  document.getElementById('result').innerHTML = currencyFormat.format(value);
+}
